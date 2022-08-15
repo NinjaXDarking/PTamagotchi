a1ea8ea8dd265daaaf95b629e3f323a059c641fe
Christopher Jose Barahona Salas <chbarahonasa@est.utn.ac.cr>
2022-08-15 00:20:02 -0600

    Se avanza la clase Actividades con sus respectivos atributos int cantidadEnergia int cantidadAburrimiento int tiempoRestante

--- src/Actividades/Actividades.java
+++ src/Actividades/Actividades.java
@@ -9,5 +9,40 @@ package Actividades;
  * @author Lenovo
  */
 public class Actividades {
+    int cantidadEnergia;
+    int cantidadAburrimiento;
+    int tiempoRestante;
+
+    public int getCantidadEnergia() {
+        return cantidadEnergia;
+    }
+
+    public void setCantidadEnergia(int cantidadEnergia) {
+        this.cantidadEnergia = cantidadEnergia;
+    }
+
+    public int getCantidadAburrimiento() {
+        return cantidadAburrimiento;
+    }
+
+    public void setCantidadAburrimiento(int cantidadAburrimiento) {
+        this.cantidadAburrimiento = cantidadAburrimiento;
+    }
+
+    public int getTiempoRestante() {
+        return tiempoRestante;
+    }
+
+    public void setTiempoRestante(int tiempoRestante) {
+        this.tiempoRestante = tiempoRestante;
+    }
+
+    public Actividades(int cantidadEnergia, int cantidadAburrimiento, int tiempoRestante) {
+        this.cantidadEnergia = cantidadEnergia;
+        this.cantidadAburrimiento = cantidadAburrimiento;
+        this.tiempoRestante = tiempoRestante;
+    }
+    
+    
     
 }
