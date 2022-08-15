bab2bc885c80d6407b59f88379c7f1ac5cba9038
Dell <Dell@192.168.1.9>
2022-08-15 01:38:37 -0600

    Se mueven las clases Alimentos,Alga,BabiriBerry,FgyBerry,GrpaBerry,TangaBerry.

---
+++ src/Alimentos/Alga.java
@@ -0,0 +1,13 @@
+package Alimentos;
+
+/**
+ *
+ * @author Dell
+ */
+public class Alga extends Alimentos {
+
+    public Alga(int cantidadEnergia, int cantidadAlimento, int tiempoRestante) {
+        super(5,5,5);
+    }
+    
+}
---
+++ src/Alimentos/GrepaBerry.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Alimentos;
+
+import Alimentos.Alimentos;
+
+/**
+ *
+ * @author Dell
+ */
+public class GrepaBerry extends Alimentos{
+
+    public GrepaBerry(int cantidadEnergia, int cantidadAlimento, int tiempoRestante) {
+        super(10,10,10);
+    }
+    
+}
