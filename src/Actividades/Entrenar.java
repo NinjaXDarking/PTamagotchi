bc1fb3de1bf87bee6c17634b52c367466d75fe61
Christopher Jose Barahona Salas <chbarahonasa@est.utn.ac.cr>
2022-08-15 00:33:17 -0600

    Se crea la clase entrenar hija de Actividades y se crea su constructor con sus respectivos atributos int cantidadEnergia int cantidadAburrimiento int tiempoRestante

---
+++ src/Actividades/Entrenar.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Actividades;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Entrenar extends Actividades {
+
+    public Entrenar(int cantidadEnergia, int cantidadAburrimiento, int tiempoRestante) {
+        super(cantidadEnergia, cantidadAburrimiento, tiempoRestante);
+    }
+    
+}
